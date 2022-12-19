c7c66d1c0d447ef09337c0459a49cd5a84efdade
Sabuto <robe_dunne@hotmail.com>
2022-12-19 21:39:48 +0000

    added new user control

---
+++ templates/csharp/usercontrol/NewUserControl.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaAppTemplate.Namespace;
+
+public partial class NewUserControl : UserControl
+{
+    public NewUserControl()
+    {
+        InitializeComponent();
+    }
+}
